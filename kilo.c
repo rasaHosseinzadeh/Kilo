540c2e180cfa169a11cea85b846c571cb572adb1
Rasa Hosseinzdaeh <rasa.hosseinzadeh@gmail.com>
2024-02-19 02:48:50 -0500

    Add raw mode - Enable raw mode in entrance. - Add hook for disabling raw mode in exit.

--- kilo.c
+++ kilo.c
@@ -1,7 +1,22 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
+
+struct termios orig_termois;
+
+void disable_raw_mode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termois); }
+
+void enable_raw_mode() {
+  tcgetattr(STDIN_FILENO, &orig_termois);
+  atexit(disable_raw_mode);
+  struct termios raw = orig_termois;
+  raw.c_lflag &= ~(ECHO | ICANON);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
   char c;
+  enable_raw_mode();
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     ;
   return 0;
