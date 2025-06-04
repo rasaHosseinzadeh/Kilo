1ad3f1c1d533d0ff7318b691c5e1d429d4a50893
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2025-06-04 18:02:45 -0400

    open first file in initial buffer

--- kilo.c
+++ kilo.c
@@ -3,12 +3,14 @@
 int main(int argc, char *argv[]) {
   init();
   if (argc >= 2) {
-    open_new_file(argv[1], 0);
+    /* populate the initial buffer with the first file */
+    open_file(argv[1]);
     for (int i = 2; i < argc; i++) {
       open_new_file(argv[i], 0);
     }
     switch_buffer(0);
   } else {
+    /* start with an empty unnamed buffer */
     open_new_file(NULL, 0);
   }
   set_status_message("This ain't vim! Hit Ctrl+q to exit.");
